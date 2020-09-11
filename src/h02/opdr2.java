6d00db1d61490c7cc98791ea8088bc745a8fe089
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-09-11 15:53:21 +0200

    opdr 2.2 sending to github.

--- src/h02/opdr2.java
+++ src/h02/opdr2.java
@@ -3,11 +3,14 @@ import java.awt.*;
 import java.applet.*;
 public class opdr2 extends Applet{
     public void init() {
-        setBackground(Color.blue);
+        setBackground(Color.white);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.yellow);
-        g.drawString("Mitchel Bunk!!", 50, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Mitchel!!", 50, 60 );
+
+        g.setColor(Color.red);
+        g.drawString("Bunk!!", 50, 120 );
     }
 }
