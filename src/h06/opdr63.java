d695fa306c2b6f0df20b09711a80ed035644b7e6
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-09-15 13:34:31 +0200

    oprd6.3: Limit Break XD (ok maar serieus. de opdracht is dat ik door op te tellen een negatief getal creeer door over het limiet te gaan)

---
+++ src/h06/opdr63.java
@@ -0,0 +1,20 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class opdr63 extends Applet {
+    int getal1, getal2, uitkomst;
+
+    public void init() {
+        getal1 = 2147483647;
+        getal2 = 600;
+        uitkomst = getal1 + getal2;
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("negatief getal" + uitkomst, 20, 20);
+        // ok het is nu duidelijk voor mij wat er gebeurd als ik bij int hoger ga dan het limiet.
+    }
+}
