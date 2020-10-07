b2a215b1deb022b42acbcf0703819ecc0c7c9f5f
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-10-07 15:48:48 +0200

    de opdrachten waar ik aan ga werken en waar ik een goede start mee heb gemaakt

---
+++ src/h10/PraktijkOpdrachtH10.java
@@ -0,0 +1,27 @@
+package h10;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PraktijkOpdrachtH10 extends Applet {
+    //TODO variabelen maken
+
+    @Override
+    public void init() {
+        //TODO tekst en label maken voor de berekening
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        setBackground(new Color(0, 105, 255, 255));
+    }
+
+    class GradeListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
