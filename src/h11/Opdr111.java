421c6d5a1b5c06d1e8d8198eda53ff25365bc29d
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-10-22 14:04:12 +0200

    11.8 alien beam

--- src/h11/Opdr111.java
+++ src/h11/Opdr111.java
@@ -10,12 +10,12 @@ public class Opdr111 extends Applet {
 
     public void paint(Graphics g) {
         int teller = 1;
-        int y = 0;
+        int y = 2;
 
-        while (teller < 11) {
-            y += 20;
-            g.drawLine(y, 50, y, 300);
-            g.drawString("" + teller, y, 315);
+        while (teller < 200) {
+            y += 2;
+            g.drawRoundRect(200, 1, y, y,y ,y);
+            g.drawString("", y, 315);
             teller++;
         }
     }
