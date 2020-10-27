461c2c4a60499d7d39f4c93c334ae033547c202f
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-10-27 17:28:45 +0100

    12.1 ik ben nu vooral de theorie en youtubefilmpjes aan het kijken die over arrays gaan maar heb wel een begin hier

---
+++ src/H12/H121.java
@@ -0,0 +1,17 @@
+package H12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H121 extends Applet {
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
