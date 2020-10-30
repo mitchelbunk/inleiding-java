971dbd4823863dc9a76b7f23a4711857b2fc9242
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-10-30 13:29:42 +0100

    opdr 12.1 gemiddelde met arrays

--- src/H12/H121.java
+++ src/H12/H121.java
@@ -4,14 +4,26 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class H121 extends Applet {
+    double[] getal;
+    double opteller, deler;
 
-    @Override
     public void init() {
-        super.init();
+        getal = new double[10];
+        deler = getal.length;
+        for (int teller = 0; teller < getal.length; teller++) {
+            getal[teller] = 50 * teller + 50;
+            opteller += 50 * teller + 50;
+        }
     }
 
-    @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        g.drawString("Het gemiddelde is: " + opteller / deler, 50, 50);
+        g.drawString("" + getal[0], 50, 100);
+        g.drawString("" + getal[1], 50, 120);
+
+        for (int indexNumber = 0, y = 70; indexNumber < getal.length; indexNumber++, y += 20){
+            g.drawString("Het volgende nummber os: " + getal[indexNumber], 50, y);
+        }
+
     }
 }
