d6652d5e963191ad8ed83bb4827906a2e8e5f365
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-11-18 16:05:47 +0100

    Opdr 13.1 af

--- src/H13/Opdr131.java
+++ src/H13/Opdr131.java
@@ -7,19 +7,18 @@ public class Opdr131 extends Applet {
 
     @Override
     public void init() {
-
     }
 
-    @Override
     public void paint(Graphics g) {
-        // lijn A naar B
-        g.drawLine(10, 11, 20, 22);
-        // lijn B naar C
-        g.drawLine(20, 22, 30, 40);
-        //lijn C naar A
-        g.drawLine(30, 40, 10, 11);
+        tekenDriehoek(g, 0, 0, 40, 0, 20, 80);
     }
-    void tekenDriehoek (Graphics g, int xA, int yA, int xB, int yB, int xC, int yC){
 
+    public void tekenDriehoek(Graphics g, int xA, int yA, int xB, int yB, int xC, int yC) {
+        // lijn A naar B
+        g.drawLine(xA, yA, xB, yB);
+        // lijn B naar C
+        g.drawLine(xB, yB, xC, yC);
+        //lijn C naar A
+        g.drawLine(xC, yC, xA, yA);
     }
 }
