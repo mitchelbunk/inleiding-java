e2322ffcbab02896887700ae6bb043fb7065edea
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-11-18 16:08:26 +0100

    Opdr 13.1 af kleine aanpassing

--- src/H13/Opdr131.java
+++ src/H13/Opdr131.java
@@ -11,6 +11,7 @@ public class Opdr131 extends Applet {
 
     public void paint(Graphics g) {
         tekenDriehoek(g, 0, 0, 40, 0, 20, 80);
+        tekenDriehoek(g, 0, 80, 40, 80, 20, 0);
     }
 
     public void tekenDriehoek(Graphics g, int xA, int yA, int xB, int yB, int xC, int yC) {
