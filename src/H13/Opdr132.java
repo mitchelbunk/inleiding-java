09c1aa77255573cc96d56d931103bf447f6273fc
mitchelbunk <mitchel.bunk@student.rocvf.nl>
2020-11-21 18:00:36 +0100

    Opdr 132 betonnen muur en bakstenen muur

---
+++ src/H13/Opdr132.java
@@ -0,0 +1,21 @@
+package H13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr132 extends Applet {
+
+
+    @Override
+    public void init() {}
+
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+    }
+
+    public void tekenBaksteen(Graphics g, int x, int y){
+
+    }
+
+
+}
